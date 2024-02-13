b363b1666594ad333a6ede8b6df4172d55bb5dd7
bhaskarlearn <159870958+bhaskarlearn@users.noreply.github.com>
2024-02-13 11:14:31 +0530

    new feature added
    new feature added

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass-strea2learn");
+        System.out.println("DevClass-strea2learn+added");
         //checkin from stram2learn
     }
 }
