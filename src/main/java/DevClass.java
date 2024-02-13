b27d69d667ebfa5004efadbb4e47296683bf9515
bhaskarlearn <159870958+bhaskarlearn@users.noreply.github.com>
2024-02-13 10:56:51 +0530

    Dash is added insted of plus symbol
    Dash is added insted of plus symbol

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,6 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass+strea2learn");
+        System.out.println("DevClass-strea2learn");
         //checkin from stram2learn
     }
 }
